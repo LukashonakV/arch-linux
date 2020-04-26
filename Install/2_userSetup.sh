c2643fbfd2f8c3efe28b0f0cf2b7e79ff2328cd0
lukashonak <myxabeer@gmail.com>
2020-04-27 00:13:51 +0300

    Create 2_userSetup.sh

---
+++ Install/2_userSetup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Turn on/off zswap
+with_zswap="1"
+
+echo "Updating packages"
+sudo pacman -Syu --noconfirm
+
+echo "Creating user's folders"
+sudo pacman -S --noconfirm xdg-user-dirs
+
+echo "Adding Vulkan support"
+sudo pacman -S --noconfirm vulkan-intel vulkan-icd-loader
+
+echo "Installing common applications"
+sudo pacman -S --noconfirm firefox openssh htop nmon p7zip ripgrep unzip
+
+echo "Installing fonts"
+sudo pacman -S --noconfirm ttf-roboto ttf-droid ttf-opensans ttf-dejavu ttf-liberation ttf-hack noto-fonts ttf-fira-code cantarell-fonts
+
+echo "Installing yay"
+cd ~/Downloads
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ../..
+rm -rf yay
+
+if [[ with_zswap -eq 1 ]]
+then
+  yay -S zramswap
+fi
