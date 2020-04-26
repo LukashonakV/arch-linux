f8f13138e1218ffcd7b3d7b6d993a1bd107f3600
lukashonak <myxabeer@gmail.com>
2020-04-27 01:01:48 +0300

    Update 1_baseInstall.sh

--- Install/1_baseInstall.sh
+++ Install/1_baseInstall.sh
@@ -29,8 +29,6 @@ with_firewall="1"
 disk_type="sda"
 #tmp to tmpfs in Gb
 tmpfs_size="0"
-#Turn on/off zswap
-with_zswap="1"
 ###############################################
 #Tech variables. Don't touch
 repo_git_path='https://raw.githubusercontent.com/lukashonak/arch-linux/master/Install'
@@ -211,12 +209,6 @@ if (( $with_firewall == 1 ))
 then
   echo "Setup FireWall"
   /Install/1_2_firewallSetup.sh '/etc/nftables.conf'
-fi
-
-if (( $with_zswap == 1 ))
-then
-  echo "Setup ZSWAP configuration"
-  /Install/1_3_zswapSetup.sh
 fi
 
 echo "Setup NetworkManager"
