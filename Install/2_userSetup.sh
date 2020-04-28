1d5a0aebfa092924d1f2e7b5ddf5fa8abbfad0db
lukashonak <myxabeer@gmail.com>
2020-04-29 00:22:04 +0300

    Update 2_userSetup.sh

--- Install/2_userSetup.sh
+++ Install/2_userSetup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Turn on/off zswap
-with_zswap="1"
-
 echo "Updating packages"
 sudo pacman -Syu --noconfirm
 
@@ -18,17 +15,3 @@ sudo pacman -S --noconfirm firefox openssh htop nmon p7zip ripgrep unzip
 
 echo "Installing fonts"
 sudo pacman -S --noconfirm ttf-roboto ttf-droid ttf-opensans ttf-dejavu ttf-liberation ttf-hack noto-fonts ttf-fira-code cantarell-fonts
-
-if [[ with_zswap -eq 1 ]]
-then
-  echo "Setup zramswap"
-  sudo swapoff -a
-  sudo cryptsetup close /dev/mapper/swap
-  cd ~/Downloads
-  git clone https://aur.archlinux.org/zramswap.git
-  cd zramswap
-  makepkg -si --noconfirm
-  cd ..
-  rm -rf zramswap  
-  sudo systemctl enable zramswap.service
-fi
