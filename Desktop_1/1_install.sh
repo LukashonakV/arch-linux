1f7da6e5680b197957f767695cf8a0c265883403
lukashonak <myxabeer@gmail.com>
2020-04-29 23:09:40 +0300

    Delete 1_install.sh

--- Desktop_1/1_install.sh
+++
@@ -1,9 +0,0 @@
-sudo pacman -S sway swaylock swayidle xorg-server-xwayland
-
-mkdir ~/.config/sway
-cp /etc/sway/config ~/.config/sway
-
-echo '# Sway autostart
-if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
-  XKB_DEFAULT_LAYOUT=us exec sway
-fi' >> ~/.bash_profile
