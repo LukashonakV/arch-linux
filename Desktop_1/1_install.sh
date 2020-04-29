89822581a29d42daa76a4bbf864684f940780eb7
lukashonak <myxabeer@gmail.com>
2020-04-29 18:49:48 +0300

    Update 1_install.sh

--- Desktop_1/1_install.sh
+++ Desktop_1/1_install.sh
@@ -2,3 +2,7 @@ sudo pacman -S sway swaylock swayidle xorg-server-xwayland
 
 mkdir ~/.config/sway
 cp /etc/sway/config ~/.config/sway
+
+echo 'if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+  XKB_DEFAULT_LAYOUT=us exec sway
+fi' >> ~/.bash_profile
