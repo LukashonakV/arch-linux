12b7984c5719e77bcff417126f937705b8c1be58
lukashonak <myxabeer@gmail.com>
2020-04-30 19:49:50 +0300

    Update 1_install.sh

--- Desktop_1/Install/1_install.sh
+++ Desktop_1/Install/1_install.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
+
+install_path="$(cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P | sed 's/\/Install//')"
+cursors_path="$install_path/Resources/Cursors"
+
 sudo pacman -S sway swaylock swayidle xorg-server-xwayland
 
-mkdir ~/.config/sway
-cp /etc/sway/config ~/.config/sway
+mkdir $XDG_CONFIG_HOME/sway
+cp /etc/sway/config $XDG_CONFIG_HOME/sway
 
 echo '# Sway autostart
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
   XKB_DEFAULT_LAYOUT=us exec sway
 fi' >> ~/.bash_profile
+
+for file in $(find cursors_path -type f);
+do
+  tar xf $file -C ~/.icons;
+done
