2141520c49f4308a9f0a42153b8a92e376b43f4c
Viktar Lukashonak <myxabeer@gmail.com>
2020-05-02 16:26:24 +0300

    Random wallpapers each boot

--- Desktop_1/Install/1_install.sh
+++ Desktop_1/Install/1_install.sh
@@ -3,11 +3,12 @@
 install_path="$(cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P | sed 's/\/Install//')"
 cursors_path="$install_path/Resources/Cursors"
 sway_path="$install_path/Resources/Sway"
+wallp_path="$install_path/Resources/Wallpapers"
 
 sudo pacman -S --noconfirm sway swaylock swayidle xorg-server-xwayland termite
 
 mkdir $XDG_CONFIG_HOME/sway
-cp -a "$sway_path""/." $XDG_CONFIG_HOME/sway
+yes | cp -arf "$sway_path""/." $XDG_CONFIG_HOME/sway
 
 echo '# Sway autostart
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
@@ -18,8 +19,15 @@ echo "Setup cursors"
 mkdir -p ~/.local/share/icons
 for file in $(find $cursors_path -type f);
 do
-  tar xf $file -C ~/.local/share/icons/;
+  tar -xf $file -C ~/.local/share/icons/;
+done
+
+echo "Setup wallpapers"
+mkdir -p ~/Pictures/Wallpapers
+for file in $(find $wallp_path -type f);
+do
+  tar -xf $file -C ~/Pictures/Wallpapers/;
 done
 
 echo "Setup GTK dark"
-echo "gtk-application-prefer-dark-theme = true" >> $XDG_CONFIG_HOME/gtk-3.0/settings.ini
+echo "gtk-application-prefer-dark-theme = true" >> "$XDG_CONFIG_HOME/gtk-3.0/settings.ini"
