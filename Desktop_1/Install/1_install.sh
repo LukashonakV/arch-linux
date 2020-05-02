8a883b50210dae7abcc9d8b78b3da1c055ffd124
Viktar Lukashonak <myxabeer@gmail.com>
2020-05-02 23:07:24 +0300

    Add missed lock script

--- Desktop_1/Install/1_install.sh
+++ Desktop_1/Install/1_install.sh
@@ -4,6 +4,7 @@ install_path="$(cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P | sed 's/\/Install
 cursors_path="$install_path/Resources/Cursors"
 sway_path="$install_path/Resources/Sway"
 wallp_path="$install_path/Resources/Wallpapers"
+usrcust_path="$install_path/Resources/UserScripts"
 
 sudo pacman -S --noconfirm sway swaylock swayidle xorg-server-xwayland termite rofi imagemagick grim
 
@@ -31,3 +32,6 @@ done
 
 echo "Setup GTK dark"
 echo "gtk-application-prefer-dark-theme = true" >> "$XDG_CONFIG_HOME/gtk-3.0/settings.ini"
+
+echo "Setup user scripts"
+yes | cp -arf "$usrcust_path""/." /usr/local/bin
