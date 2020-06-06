42f61e1850bac66f2c7271f228c95c392a1ce504
Viktar Lukashonak <myxabeer@gmail.com>
2020-06-07 01:04:26 +0300

    Waybar. Currency module interval - 30 minutes. New format

--- Desktop_1/Resources/Waybar/DATA/getCurrency.sh
+++ Desktop_1/Resources/Waybar/DATA/getCurrency.sh
@@ -13,10 +13,30 @@ getIcon() {
 getRate() {
     local __resultRate=$2
     local __resultTooltip=$3
-    local request=$(curl -s "https://www.nbrb.by/api/exrates/rates/$1?parammode=2")
 
-    eval $__resultRate="'$(getIcon "$1")" "$(echo "$request" | jq .Cur_OfficialRate)'"
-    eval $__resultTooltip="'$(echo "$request" | jq '((.Cur_OfficialRate|tostring) + " " + .Cur_Abbreviation)')'"
+    local request=$(curl -s "https://www.nbrb.by/api/exrates/rates/$1?parammode=2")
+    local Cur_OfficialRate=$(echo "$request" | jq .Cur_OfficialRate)
+  
+    eval $__resultRate="'$(getIcon "$1") $Cur_OfficialRate'"
+    eval $__resultTooltip="'$1 $Cur_OfficialRate'"
+
+    local ondate="&ondate="$(date -d "1 day" +"%Y-%m-%d")
+    local request_ondate=$(curl -s "https://www.nbrb.by/api/exrates/rates/$1?parammode=2$ondate")
+    local Cur_OfficialRate_onDate=$(echo "$request_ondate" | jq .Cur_OfficialRate)
+
+    if [[ $(echo $Cur_OfficialRate | sed 's/\./,/') -gt $(echo $Cur_OfficialRate_onDate | sed 's/\./,/' ) ]]; then
+        eval $__resultRate+=""
+        eval $__resultTooltip+=""
+    fi
+
+    if [[ $(echo $Cur_OfficialRate | sed 's/\./,/') -lt $(echo $Cur_OfficialRate_onDate | sed 's/\./,/' ) ]]; then
+        eval $__resultRate+=""
+        eval $__resultTooltip+=""
+    fi
+
+    if [[ $(echo $Cur_OfficialRate | sed 's/\./,/') -ne $(echo $Cur_OfficialRate_onDate | sed 's/\./,/' ) ]]; then
+        eval $__resultTooltip+="'$Cur_OfficialRate_onDate'"
+    fi
 }
 
 result=""
@@ -27,7 +47,6 @@ shift
 while [ -n "$1" ]
 do
     getRate $1 tempresult temptooltip
-    result+="|$tempresult"
     tooltip+="|$temptooltip"
     shift
 done
